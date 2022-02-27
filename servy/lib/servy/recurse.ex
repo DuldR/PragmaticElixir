fdbc1c8d54d5ffc49fcf43f5979d87e5e748bfd0
Garrett <gnjulaka@gmail.com>
2022-02-26 20:41:47 -0600

    Recursion isn't my strong suit

--- servy/lib/servy/recurse.ex
+++ servy/lib/servy/recurse.ex
@@ -9,26 +9,24 @@ defmodule Recurse do
 
   def loopy([], total), do: total
 
-
-#   def triple([head | tail], returnList) do
-#     IO.puts "Head: #{inspect(head)} Tail: #{inspect(tail)}"
-
-#     triple(tail, returnList ++ [head * 3])
-
-#   end
-
-#   def triple([], returnList) do
-#       IO.puts "#{inspect(returnList)}"
-#   end
-
   def triple([head|tail]) do
     [head*3 | triple(tail)]
   end
 
   def triple([]), do: []
+
+  def my_map([head | tail], fun) do
+
+    [fun.(head) | my_map(tail, fun)]
+  end
+
+  def my_map([], _fun), do: []
+
 end
 
-IO.inspect Recurse.triple([1, 2, 3, 4, 5])
+# IO.inspect Recurse.triple([1, 2, 3, 4, 5])
+
+IO.inspect Recurse.my_map([1, 2, 3, 4, 5], &(&1 * 4))
 
 # IO.puts Recurse.loopy([1, 2, 3, 4, 5], 0)
 # IO.puts Recurse.triple([1, 2, 3, 4, 5], [])
